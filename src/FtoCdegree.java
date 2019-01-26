65637b6639830898893150771e0e8f4fd4396bae
Dell <Dell@Dell-PC.home>
2019-01-26 23:13:51 +0200

    first changes

---
+++ src/FtoCdegree.java
@@ -0,0 +1,20 @@
+
+public class FtoCdegree {
+
+	public static void main(String[] args) {
+		   double f=10,c=24;
+	        double result1=celsiusToFahrenheit(c);
+	        double result2= fahrenheitToCelsius(f);
+	        System.out.println("convert celsius"+result1);
+	         System.out.println("convert fahrenheit"+result2);
+
+	}
+    public static double celsiusToFahrenheit(double celsius){
+        return (9.0/5)*celsius+32;
+    }
+    public static double  fahrenheitToCelsius(double fahrenheit){
+        return ((fahrenheit-32)*9)/5.0;
+    }
+	
+
+}
