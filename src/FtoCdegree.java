a94410aa1d6bc774937f1094239a56da9337fe9f
Dell <Dell@Dell-PC.home>
2019-02-01 20:48:48 +0200

    this edit contain add documentation

--- src/FtoCdegree.java
+++ src/FtoCdegree.java
@@ -1,7 +1,11 @@
 
+/**
+ * this class convert the value of tempreture  by using two methods 
+ */
 public class FtoCdegree {
 
 	public static void main(String[] args) {
+		
 		   double f=10,c=24;
 	        double result1=celsiusToFahrenheit(c);
 	        double result2= fahrenheitToCelsius(f);
@@ -9,12 +13,23 @@ public class FtoCdegree {
 	         System.out.println("convert fahrenheit"+result2);
 
 	}
+	/**
+    	 * Method to convert the degree from celsius to fahrenheit 
+    	 * @param celsius old value of degree
+    	 * @return the new value
+    	 */
     public static double celsiusToFahrenheit(double celsius){
+    	
         return (9.0/5)*celsius+32;
     }
-    //this for second edit
-    
+  
+    	/**
+    	 * Method to convert the degree from fahrenheit 
+    	 * @param fahrenheit old value of degree 
+    	 * @return the new value
+    	 */
     public static double  fahrenheitToCelsius(double fahrenheit){
+    
         return ((fahrenheit-32)*9)/5.0;
     }
 	
